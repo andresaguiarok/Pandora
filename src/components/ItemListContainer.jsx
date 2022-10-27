61cb470022787c81386dbcf32bff2aa2f35a9da8
Andres Aguiar <aguiarandresok@outlook.es>
2022-10-26 21:51:13 -0300

    preentrega-aguiar

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./styles/itemlist.css";
 import Diamante from "../images/diamante1.gif";
 
-export default function BodyApp({greeting}){
+export default function ItemListContainer({greeting}){
     return (
-        <div className="fondo">
+        <div className="goteoFondo container-fluid">
             <div className="text-center">
                 <div className="container h1" style={{background: "rgba(0, 0, 0, 0.418)"}}>{greeting}</div>
             <img src={Diamante} alt="diamante" className="img-fluid" />
