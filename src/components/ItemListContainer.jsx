d8fada52c77cdd27723286b69d212d8be73fac77
Andres Aguiar <aguiarandresok@outlook.es>
2022-10-26 23:16:07 -0300

    PreEntrega-Aguiar

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles/itemlist.css";
 import Diamante from "../images/diamante1.gif";
+import Fondo from "../images/fondo-slime.jpg";
 
 export default function ItemListContainer({greeting}){
     return (
-        <div className="goteoFondo container-fluid">
+        <div style={{
+            backgroundImage: `url(${Fondo})`, backgroundSize: "cover", height:"60vw", color: "white",}}>
             <div className="text-center">
                 <div className="container h1" style={{background: "rgba(0, 0, 0, 0.418)"}}>{greeting}</div>
             <img src={Diamante} alt="diamante" className="img-fluid" />
