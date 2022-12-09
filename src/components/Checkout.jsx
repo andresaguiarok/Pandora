a0f803c431959362051c03d079d0f959f6303458
Andres Aguiar <aguiarandresok@outlook.es>
2022-12-08 22:29:43 -0300

    eliminacion de imagenes

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -7,9 +7,9 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import { Link } from 'react-router-dom';
 import Swal from "sweetalert2";
+import Gracias from "../images/gracias-por-tu-compra.png";
 import { cartContext } from "./CartContext";
 import "./styles/carrito.css";
-import Gracias from "../images/gracias-por-tu-compra.png";
 
 function Checkout() {
 
